a06c6c8a094f6bdc4f73bc5ff5a21cf8515ec3fd
Nacho <nacho.mk@hotmail.com>
2024-02-24 03:16:36 -0300

    cambios navbar

--- Preentregas/navbarproject/src/App.jsx
+++ Preentregas/navbarproject/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
 
     <Navbar/>
+    <ItemListContainer greeting="Hola"/>
 
     </>
   )
---
+++ Preentregas/navbarproject/src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = ({ greeting }) => {
+    return (
+        <div className="text-center">
+            <span>{greeting}</span>
+        </div>
+    )
+}
+
+export default ItemListContainer ;
