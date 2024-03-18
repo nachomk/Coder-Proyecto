bd6ac6cc41025f98ed7003854486f0cec4a5294f
Nacho <nacho.mk@hotmail.com>
2024-03-18 14:35:11 -0300

    se agrego productos en categorias

--- Proyecto/src/components/ItemListContainer.jsx
+++ Proyecto/src/components/ItemListContainer.jsx
@@ -46,6 +46,7 @@ const ItemListContainer = () => {
         productsPorCategoria && (
             productsPorCategoria.map((producto) => (
                 <Link key={producto.id} to={`/item/${producto.id}`}>    
+                  <h2>{producto.nombre}</h2>
                 </Link>
         ))
     )
