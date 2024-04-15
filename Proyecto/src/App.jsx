4b93a7a94696f099bb877ff44dc4cc9bf7179a36
Nacho <nacho.mk@hotmail.com>
2024-04-15 00:26:05 -0300

    toastify en carrito

--- Proyecto/src/App.jsx
+++ Proyecto/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './components/NavBar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ItemListContainer from './components/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer'
-import { CartContext, CartProvider } from './context/CartContext'
+import { CartProvider } from './context/CartContext'
 import { ToastContainer } from 'react-toastify'
 
 function App() {
